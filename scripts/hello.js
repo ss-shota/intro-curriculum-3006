44231ac9ebfc0875efe4db37c062a72517cb59bd
Shota sakaguchi <shota211ss@yahoo.co.jp>
2022-03-15 06:42:36 +0900

    おみくじ機能追加-2

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = robot => {
   robot.hear(/hello>/i, msg => {
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
+    msg.send(`&{lots}, <@${user_id}>`);
   });
 };
